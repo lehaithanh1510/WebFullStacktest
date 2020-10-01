eab48daf1ca1f8924fee910dad840a38808b2852
Le Hai Thanh <haithuthanhtien@gmail.com>
2020-10-02 00:53:00 +0800

    fixed commit

--- A/A2/A2.js
+++ A/A2/A2.js
@@ -1,44 +1,57 @@
-const replace = (a,b) => {
-    let temp  ;
-    temp = b ;
-    b=a;
-    a=temp;
-}
+
 const sortRank = (arr = []) => {
-    for (let i=arr.length;i>=0;i--) {
-        for (let j=1; j <arr.length ;j++){
-            // console.log(arr[j])
-            if (arr[j-1].point > arr[j].point) {
-                replace(arr[j-1],arr[j])
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = arr.length - 1; j > i; j--) {
+            if (arr[j - 1].point < arr[j].point) {
+                let temp = undefined;
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
             }
-            if (arr[j-1].point === arr[j].point && arr[j-1].GD < arr[j].GD) replace(arr[j-1],arr[j])
-
-             if (arr[j-1].point === arr[j].point && arr[j-1].GD === arr[j].GD && arr[j-1].name < arr[j].name) {
-                replace(arr[j-1],arr[j])
+            else if (arr[j - 1].point === arr[j].point && arr[j-1].GD!== arr[j].GD) {
+                if (arr[j].GD < arr[j - 1].GD) {
+                    let temp = undefined;
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
             }
 
+            else if (arr[j - 1].point === arr[j].point && arr[j - 1].GD === arr[j].GD) {
+                console.log(arr[j - 1].name, arr[j].name)
+                if (arr[j - 1].name > arr[j].name) {
+                    let temp = undefined;
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
     }
     return arr
 }
-console.log(sortRank([{
-    name:"Arsenal",
-    point:99,
-    GD:39,
-   },
-   {
-    name:"Chelsea",
-    point:75,
-    GD:29,
-   },
-   {
-    name:"Liverpool",
-    point:75,
-    GD:22,
-   },
-   {
-    name:"Manchester United",
-    point:99,
-    GD:39,
-   },
-]))
+console.log(sortRank(
+    [
+
+        {
+            name: "Chelsea",
+            point: 75,
+            GD: 29,
+        },
+        {
+            name: "Liverpool",
+            point: 75,
+            GD: 22,
+        },
+        {
+            name: "Manchester United",
+            point: 99,
+            GD: 39,
+        },
+        {
+            name: "Arsenal",
+            point: 99,
+            GD: 39,
+        },
+    ]
+))
--- B/B.js
+++ B/B.js
@@ -6,29 +6,84 @@ fetch('https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=mul
     .then(data => {
         question = data
         console.log(question)
+        webController = {
+            init :function() {
+                view.renderQuestion()
+                document.getElementById("quiz_form").addEventListener('submit',(e) => {
+                    e.preventDefault()
+                    const listOptions = document.querySelectorAll("input")
+                    console.log(listOptions)
+                    const listCheckedAnswers = []
+                    for (option of  listOptions) {
+                        if (option.className === "correct_answer") {
+                            document.getElementById(option.id).nextElementSibling.classList.add('right')
+                        }
+                        if (option.checked) listCheckedAnswers.push(option)  
+                    }
+                    console.log(listCheckedAnswers)
+                    for (let i=0 ;i<listCheckedAnswers.length ;i++) {
+                        if (listCheckedAnswers[i].className === "incorrect_answer") {
+                            document.getElementById(listCheckedAnswers[i].id).nextElementSibling.classList.add('wrong')
+                        }
+                        // if (listCheckedAnswers[i].className === "correct_answer") {
+                        //     document.getElementById(listCheckedAnswers[i].id).nextElementSibling.classList.add('right')
+                        // }
+                    }
+                })
+                document.querySelector(".do_again").addEventListener('click',() => { 
+                    document.querySelector(".quiz_wrapper").innerHTML=''
+                    view.renderQuestion()
+                })
+
+
+            }
+        }
         view = {
             renderQuestion: function () {
                 for (let i = 0; i < question.results.length; i++) {
-                    htmldoc = `
-                <div class="question_header"> Câu %number% </div>
-                <div class="question">%question%</div>
-                <label for="answer1" class="answer"><input id="answer1"  type="radio" name="1"> %answer1%</label><br>
-                <label for="answer2" class="answer"><input id="answer2"  type="radio" name="1"> %answer2%</label><br>
-                <label for="answer3" class="answer"><input id="answer3"  type="radio" name="1"> %answer3%</label><br>
-                <label for="answer4" class="answer"><input id="answer4"  type="radio" name="1"> %answer4%</label><br>
-                `
-                var newHtml = htmldoc.replace("%number%",i)
-                newHtml = newHtml.replace("%question%",question.results[i].question)
-                newHtml = newHtml.replace("%answer1%",question.results[i].correct_answer)
-                newHtml = newHtml.replace("%answer2%",question.results[i].incorrect_answers[0])
-                newHtml = newHtml.replace("%answer3%",question.results[i].incorrect_answers[1])
-                newHtml = newHtml.replace("%answer4%",question.results[i].incorrect_answers[2])
-                document.querySelector(".football_form").insertAdjacentHTML('beforeend',newHtml)
-        
+                    const questionWrapper = document.createElement("div")
+                    questionWrapper.classList.add("question_wrapper")
+                    const questionHeader = document.createElement("div")
+                    questionHeader.classList.add("question_header")
+                    questionHeader.innerText = `Câu ${i + 1}`
+                    questionWrapper.appendChild(questionHeader)
+                    const questionContent = document.createElement("div")
+                    questionContent.classList.add("question")
+                    questionContent.innerText = question.results[i].question
+                    questionWrapper.appendChild(questionContent)
+                    const listAnswers = document.createElement("ul")
+                    listAnswers.classList.add("list_answers")
+                    questionWrapper.appendChild(listAnswers)
+                    for (let j = 0; j < 3; j++) {
+                        const answerWrapper = document.createElement("li")
+                        const incorrectAnswer = document.createElement("input")
+                        incorrectAnswer.type = "radio"
+                        incorrectAnswer.classList.add("incorrect_answer")
+                        incorrectAnswer.id = `question${i}_answer${j+1}`
+                        incorrectAnswer.name = `${i}`
+                        const incorrectAnswerContent = document.createElement("label")
+                        incorrectAnswerContent.innerText = question.results[i].correct_answer
+                        incorrectAnswerContent.innerText = question.results[i].incorrect_answers[j]
+                        answerWrapper.appendChild(incorrectAnswer)
+                        answerWrapper.appendChild(incorrectAnswerContent)
+                        listAnswers.appendChild(answerWrapper)
+                    }
+                    const correctAnswerWrapper = document.createElement("li")
+                    const correctAnswer = document.createElement("input")
+                    correctAnswer.type = "radio"
+                    correctAnswer.classList.add("correct_answer")
+                    correctAnswer.id = `question${i}_answer4`
+                    correctAnswer.name = `${i}`
+                    const correctAnswerContent = document.createElement("label")
+                    correctAnswerContent.innerText = question.results[i].correct_answer
+                    correctAnswerWrapper.appendChild(correctAnswer)
+                    correctAnswerWrapper.appendChild(correctAnswerContent)
+                    listAnswers.appendChild(correctAnswerWrapper)
+                    document.querySelector(".quiz_wrapper").appendChild(questionWrapper)
                 }
-            }
+            },
+            
         }
-        view.renderQuestion()
+        webController.init()
 
-    }
-    )
+    })
